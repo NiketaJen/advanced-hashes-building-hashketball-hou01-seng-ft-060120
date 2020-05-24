9eb3d973971c4af4f50047e39d9d07f438e70790
Niketa Jenerette <niketa.jenerette@gmail.com>
2020-05-24 02:02:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,28 +3,28 @@ def game_hash
   game_hash={
     :home =>{:team_name =>"", 
       :colors => [""],
-      :players => [{:player_name =>},
-      {:number =>}, 
-      {:shoe =>},
-      {:points =>},
-      {:rebounds =>},
-      {:assists =>},
-      {:steals =>},
-      {:blocks =>},
-      {:slam_dunks =>}
+      :players => [{:player_name =>""},
+      {:number =>""}, 
+      {:shoe =>""},
+      {:points =>""},
+      {:rebounds =>""},
+      {:assists =>""},
+      {:steals =>""},
+      {:blocks =>""},
+      {:slam_dunks =>""}
       }]
     },
   :away =>{:team_name => "",
-    :colors => [],
-    :players => [{:player_name =>},
-      {:number =>}, 
-      {:shoe =>},
-      {:points =>},
-      {:rebounds =>},
-      {:assists =>},
-      {:steals =>},
-      {:blocks =>},
-      {:slam_dunks =>}
+    :colors => [""],
+    :players => [{:player_name =>""},
+      {:number =>""}, 
+      {:shoe =>""},
+      {:points =>""},
+      {:rebounds =>""},
+      {:assists =>""},
+      {:steals =>""},
+      {:blocks =>""},
+      {:slam_dunks =>""}
     }]
   }}
 end
